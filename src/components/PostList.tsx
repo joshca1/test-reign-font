f76396e415db157734631294f77f58f1cabf2014
jose <jose@asimov.cl>
2021-12-12 21:17:51 -0300

    feat: add icon to post item.

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -6,10 +6,16 @@ interface Post {
     story_title: string;
     story_url: string;
     created_at: Date;
+    fav?: boolean;
 }
 
 interface Props {
     posts: any
+}
+const addPostToFav = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    console.log(e.currentTarget.dataset.id);
+    
 }
 
 const postList: React.FC<Props> = ({ posts }) => {
@@ -17,14 +23,16 @@ const postList: React.FC<Props> = ({ posts }) => {
         <div className="post-list">
             {posts.map((post: Post) => {
                 return (
-                    <div className="post-item-container" key={post.story_id}>
+                    <div className="post-item-container" key={`${post.story_id}-${post.created_at}`}>
                         <a className="post-item" href={post.story_url} rel="noreferrer" target="_blank">
                             <div className="post-item-info">
                                 <p className="post-date"> <img src="clock.svg" alt="clock icon" />{post.created_at}</p>
                                 <h2 className="post-item-title">{post.story_title}</h2>
                             </div>
                             <div className="post-item-fav">
-                                <FavIcon active={true}/>
+                                <div data-id={`${post.story_id}-${post.created_at}`} onClick={addPostToFav}>
+                                    <FavIcon  active={true}/>
+                                </div>
                             </div>
                         </a>
                     </div>
