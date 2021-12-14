e3480ee09d085f9584a89081a1896de3e4133df9
jose <jose@asimov.cl>
2021-12-13 21:39:57 -0300

    chore: add global post interface.

--- src/models/news.tsx
+++ src/models/news.tsx
@@ -4,3 +4,14 @@ export interface FetchPostOptions {
   topic: NewsTopics;
   page?: number;
 }
+
+export interface Post {
+  story_id: number;
+  author: string;
+  story_title: string;
+  story_url: string;
+  created_at: Date;
+  created_at_i: number;
+  fav?: boolean;
+}
+
